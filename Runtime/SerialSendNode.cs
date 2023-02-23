c4164a0a275f433403f325f4236410413caf415e
Felix Groll <felix.groll@gmail.com>
2023-02-23 21:48:00 +0100

    📎 added blank SerialSendNode

---
+++ Runtime/SerialSendNode.cs
@@ -0,0 +1,14 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace ƒx.SimpleSerial
+{
+    [UnitTitle("Serial Send")]
+    [UnitCategory("SimpleSerial")]
+    public class SerialSendNode : Unit
+    {
+        protected override void Definition()
+        {
+        }
+    }
+}
